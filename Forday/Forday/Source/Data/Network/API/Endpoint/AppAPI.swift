7952d82b009a2db5e62a634e5b4e5377f4c7e3ed
realhsb <realhsb@gmail.com>
2026-01-15 13:33:22 +0900

    feat: App 리소스 다운 (취미선택)

--- Forday/Forday/Source/Data/Network/API/Endpoint/AppAPI.swift
+++ Forday/Forday/Source/Data/Network/API/Endpoint/AppAPI.swift
@@ -11,4 +11,17 @@ enum AppAPI {
     case fetchAppMetadata         /// 앱 리소스 다운로드
     case fetchPresignedUrl        /// presigned url 발급
     case fetchAlarm               /// 알림 모아보기
+    
+    var endpoint: String {
+        switch self {
+        case .fetchAppMetadata:
+            return "/app/metadata"
+            
+        case .fetchPresignedUrl:
+            return ""
+            
+        case .fetchAlarm:
+            return ""
+        }
+    }
 }
--- Forday/Forday/Source/Data/Network/TargetType/AppTarget.swift
+++ Forday/Forday/Source/Data/Network/TargetType/AppTarget.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import Moya
+import Alamofire
+
+enum AppTarget {
+    case fetchAppMetadata
+}
+
+extension AppTarget: BaseTargetType {
+    
+    var baseURL: URL {
+        return URL(string: APIConstants.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .fetchAppMetadata:
+            return AppAPI.fetchAppMetadata.endpoint
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .fetchAppMetadata:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .fetchAppMetadata:
+            return .requestPlain
+        }
+    }
+}
