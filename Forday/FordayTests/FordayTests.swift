61b0189579c52b2f0ffe46759d301045ee7f8373
realhsb <realhsb@gmail.com>
2026-02-11 16:55:23 +0900

    [#80] docs: Swift Testing 세팅

---
+++ Forday/FordayTests/FordayTests.swift
@@ -0,0 +1,16 @@
+//
+//  FordayTests.swift
+//  FordayTests
+//
+//  Created by Subeen on 2/10/26.
+//
+
+import Testing
+
+struct FordayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
