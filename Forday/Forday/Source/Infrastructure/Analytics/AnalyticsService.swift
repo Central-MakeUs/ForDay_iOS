27cc15ffde1bf6addaf2e1aeab70af73a86f3db0
realhsb <realhsb@gmail.com>
2026-03-05 18:18:32 +0900

    [#109] fix: log 메서드명 변경

--- Forday/Forday/Source/Infrastructure/Analytics/AnalyticsService.swift
+++ Forday/Forday/Source/Infrastructure/Analytics/AnalyticsService.swift
@@ -10,7 +10,7 @@ import Foundation
 /// Analytics 서비스 프로토콜
 protocol AnalyticsService {
     /// 이벤트 전송
-    func trackEvent(_ event: AnalyticsEvent)
+    func log(_ event: AnalyticsEvent)
 
     /// 사용자 속성 설정
     func setUserProperty(value: String?, forName name: String)
